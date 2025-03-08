81e604c4f97e58783fb7cfa028f881094f3b488b
mansi3013gupta <mansigupta3013@gmail.com>
2025-03-08 16:25:53 +0530

    new project

---
+++ expense-tracker/app/page.js
@@ -0,0 +1,15 @@
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+      <Header/>
+      <Hero/>
+      
+    </div>
+    
+  );
+}
---
+++ expense-tracker/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image';
+import { Button } from './ui/button';
+export default function Header() {
+  return (
+    <div className='p-5 flex justify-between items-center border shadow-sm'>
+      <Image src='./logo.svg' alt="logoo" width={160} height={100}/> 
+      <Button>Get Started</Button>
+    </div>
+  )
+}
+
+
