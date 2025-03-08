b616bae201c3b3525baabf53912b9dca09802281
aditi <aditijeena1122@gmail.com>
2025-03-08 19:55:14 +0530

    Changes

--- expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
+++ expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
-
+import Image from "next/image";
+import {LayoutGrid,PiggyBank,ReceiptText,ShieldCheck} from 'lucide-react'
+import { UserButton } from '@clerk/nextjs';
 function SideNav()
 {
+    const menulist=[
+        {
+            id:1,
+            name:'Dashboard',
+            icon:LayoutGrid
+        },
+
+        {
+            id:2,
+            name:'Budgets',
+            icon:PiggyBank
+        },
+
+        {
+            id:3,
+            name:'Expenses',
+            icon:ReceiptText
+        },
+
+        {
+            id:4,
+            name:'Upgrade',
+            icon:ShieldCheck
+        }
+    ]
     return (
-        <div>SideNav</div>
+        <div className='h-screen p-5 border shadow-sm'>
+            <Image src={'/logo.svg'}
+            alt='logo'
+            width={160}
+            height={100}
+            />
+
+            <div className='mt-5'>
+               
+               {menulist.map((menu,index)=>(
+                <h2 className='flex gap-2 items-center
+                text-gray-500 font-medium
+                p-5 cursor-pointer rounded-md
+                hover:text-primary hover:bg-blue-100'>
+                    <menu.icon/>
+                    {menu.name}
+                </h2>
+               )
+            
+            )}
+            </div>
+        </div>
     )
 }
 
