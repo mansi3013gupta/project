a091e68f92e3c87ebd0d7ca7124be3115cccf51b
aditi <aditijeena1122@gmail.com>
2025-03-08 23:09:04 +0530

    Commit1

--- expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
+++ expense-tracker/app/(routes)/dashboard/_components/SideNav.jsx
@@ -51,6 +51,7 @@ function SideNav()
             
             )}
             </div>
+            <UserButton/>
         </div>
     )
 }
--- expense-tracker/app/page.js
+++ expense-tracker/app/page.js
@@ -1,5 +1,5 @@
 
-import { Button } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
