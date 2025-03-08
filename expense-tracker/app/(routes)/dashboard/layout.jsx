c8f16c1631a6a8facb66a578bb7c3923baa458ef
aditi <aditijeena1122@gmail.com>
2025-03-08 17:44:28 +0530

    com2

---
+++ expense-tracker/app/(routes)/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SideNav from './_components/SideNav'
+function DashboardLayout({children})
+{
+    return (
+
+      <div>
+        <div className='fixed md:w-64 hidden md:block bg-red-100'>
+            <SideNav/>
+        </div>
+
+        <div className='md:ml-64 bg-green-200'>
+            {children}
+        </div>
+      </div>
+    )
+}
+
+export default DashboardLayout
